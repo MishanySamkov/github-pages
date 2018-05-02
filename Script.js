2541cd30898681a1c065b08c605b1b316bf51652
Mike <mixail.samkov@yandex.ru>
2018-05-02 15:00:18 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+﻿menu.onclick = function func() {
+	var m = document.getElementById("mytopnav");
+	
+	if (m.className == "topnav") {
+		m.className += " responsive";
+		
+	} else {
+		m.className = "topnav";
+	} 
+}
